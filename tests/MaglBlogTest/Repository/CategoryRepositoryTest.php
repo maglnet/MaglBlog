efb2c01b5ff2ad20f256cf9b98509a4c1bcc4628
Matthias Glaub <magl@magl.net>
2014-04-02 00:44:12 +0200

    readding the controller / repository tests

---
+++ tests/MaglBlogTest/Repository/CategoryRepositoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace MaglBlogTest\Repository;
+
+/**
+ * Description of CategoryRepositoryTest
+ *
+ * @author matthias
+ */
+class CategoryRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+	
+	/**
+	 *
+	 * @var \MaglBlog\Repository\CategoryRepository
+	 */
+	private $catRepo;
+	
+//	public function setUp(){
+//		$this->catRepo = \MaglBlogTest\Bootstrap::getServiceManager()->get('MaglBlog\CategoryRepository');
+//	}
+//	
+//	public function testEmptyCategories(){
+//		
+//		$cats = $this->catRepo->findAll();
+//		
+//		$this->assertEmpty($cats);
+//		
+//	}
+	
+}
